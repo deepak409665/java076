1a4a2b5bc4373292bf923af130ec47a4658799f7
java076 <deepak@409665>
2025-09-02 11:35:19 +0530

    Array - 02-09-2025

---
+++ src/UNIT3/array.java
@@ -0,0 +1,12 @@
+package UNIT3;
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        Scanner sc= new Scanner(System.in);
+        for(int i =0 ; i < a.length;i++){
+            a[i]= sc.nextInt();
+        }
+    }
+}
